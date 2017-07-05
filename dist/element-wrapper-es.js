71f5d01a8091bb16af6a5330d85d5f610a360628
Mickael Vieira <contact@mickael-vieira.com>
2017-07-05 20:46:31 +0100

    Simplify rollup config, remove minified version and add es version

---
+++ dist/element-wrapper-es.js
@@ -0,0 +1,121 @@
+/**
+* @param {Element} element
+* @param {Object}  props
+*
+* @returns {Element}
+*/
+var applyProperties = function (element) {
+  var props = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
+
+  Object.keys(props).forEach(function (prop) {
+    return element[prop] = props[prop];
+  });
+  return element;
+};
+
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+
+/**
+* Try to create a node with the value provided
+* otherwise it returns a text node containing the name provided
+*
+* @param {string} value
+*
+* @returns {Node}
+*/
+function tryNodeName(value) {
+  try {
+    return document.createElement(value);
+  } catch (e) {
+    return document.createTextNode(value);
+  }
+}
+
+/**
+* Create and wrap an Element in order to chain its methods
+*
+* @param {String} name
+* @param {Object} props
+*
+* @returns {Proxy}
+*/
+var createWrapper = function (name) {
+  var props = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
+
+  var element = applyProperties(document.createElement(name), props);
+
+  element.appendTextNode = function (text) {
+    this.appendChild(document.createTextNode(text));
+  };
+
+  element.appendNode = function (name) {
+    var props = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
+
+    this.appendChild(applyProperties(document.createElement(name), props));
+  };
+
+  element.appendWrappers = function () {
+    var _this = this;
+
+    for (var _len = arguments.length, wrappers = Array(_len), _key = 0; _key < _len; _key++) {
+      wrappers[_key] = arguments[_key];
+    }
+
+    wrappers.map(function (wrapper) {
+      return _this.appendChild(wrapper.unwrap());
+    });
+  };
+
+  element.unwrap = function () {
+    return this;
+  };
+
+  element.appendChildren = function () {
+    var _this2 = this;
+
+    for (var _len2 = arguments.length, children = Array(_len2), _key2 = 0; _key2 < _len2; _key2++) {
+      children[_key2] = arguments[_key2];
+    }
+
+    children.map(function (child) {
+      if (typeof child === "string") {
+        _this2.appendChild(tryNodeName(child));
+      } else if (Array.isArray(child)) {
+        _this2.appendNode(child[0], child[1]);
+      } else {
+        _this2.appendChild(child);
+      }
+    });
+  };
+
+  return new Proxy(element, {
+    get: function get(target, name, receiver) {
+      if (!(name in target)) {
+        throw new Error("Invalid method or property name \"" + name + "\"");
+      }
+
+      if (name === "unwrap") {
+        return function () {
+          return target;
+        };
+      }
+
+      if (typeof target[name] !== "function") {
+        return target[name];
+      }
+
+      return function () {
+        var args = Array.from(arguments);
+        var result = target[name].apply(target, _toConsumableArray(args));
+        return (/^(get|has)/.test(name) ? result : receiver
+        );
+      };
+    },
+    set: function set(target, prop, value, receiver) {
+      target[prop] = value;
+      return receiver;
+    }
+  });
+};
+
+export { createWrapper, applyProperties };
--- rollup.config.js
+++ rollup.config.js
@@ -1,30 +1,20 @@
 import babel from "rollup-plugin-babel";
-import uglify from "rollup-plugin-uglify";
 
-const isProd = process.env.NODE_ENV === "production";
-
-const format = "umd";
-const exports = "named";
-const moduleName = "elementWrapper";
-const sourceMap = false;
 const plugins = [
   babel({
     exclude: "node_modules/**"
   })
 ];
 
-if (isProd) {
-  plugins.push(uglify());
-}
-
-const min = isProd ? ".min" : "";
-
 export default {
   entry: "src/index.js",
-  dest: `dist/element-wrapper${min}.js`,
-  moduleName,
-  exports,
-  format,
+  moduleName: "elementWrapper",
+  exports: "named",
+  format: "umd",
   plugins,
-  sourceMap
+  sourceMap: false,
+  targets: [
+    { dest: `dist/element-wrapper.js`, format: "umd" },
+    { dest: `dist/element-wrapper-es.js`, format: "es" }
+  ]
 };
