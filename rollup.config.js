7a31b7a347718a59ae80d4f24a22c08419b1fdbe
Mickael Vieira <contact@mickael-vieira.com>
2018-04-04 19:13:53 +0100

    Fix rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -2,19 +2,32 @@ import babel from "rollup-plugin-babel";
 import uglify from "rollup-plugin-uglify";
 import { minify } from "uglify-es";
 
+const sourceMap = false;
+const exports = "named";
+const name = "DOMElementWrapper";
+
 export default {
   input: "src/index.js",
-  name: "DOMElementWrapper",
-  exports: "named",
   plugins: [
     babel({
       exclude: "node_modules/**"
     }),
     uglify({}, minify)
   ],
-  sourceMap: false,
   output: [
-    { file: `dist/dom-element-wrapper.js`, format: "umd" },
-    { file: `dist/dom-element-wrapper-es.js`, format: "es" }
+    {
+      sourceMap,
+      exports,
+      name,
+      file: `dist/dom-element-wrapper.js`,
+      format: "umd"
+    },
+    {
+      sourceMap,
+      exports,
+      name,
+      file: `dist/dom-element-wrapper-es.js`,
+      format: "es"
+    }
   ]
 };
